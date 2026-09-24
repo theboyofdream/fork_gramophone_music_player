46bf31f91930e32d1466fd3900887132be549e46
Gramophone <developer@gramophone.app>
2026-09-24 22:48:30 +0530

    fix(crash): implement onError in NoPlaceholderImageViewTarget to safely render error drawables in Coil 3

--- app/src/main/java/org/akanework/gramophone/ui/components/NoPlaceholderViewTarget.kt
+++ app/src/main/java/org/akanework/gramophone/ui/components/NoPlaceholderViewTarget.kt
@@ -20,15 +20,21 @@ package org.akanework.gramophone.ui.components
 import android.widget.ImageView
 import coil3.Image
 import coil3.ImageLoader
+import coil3.asDrawable
 import coil3.imageLoader
 import coil3.request.Disposable
 import coil3.request.ImageRequest
-import coil3.request.target
 import coil3.target.ImageViewTarget
 
 class NoPlaceholderImageViewTarget(view: ImageView) : ImageViewTarget(view) {
     override fun onStart(placeholder: Image?) {
         // do nothing
+    }
+
+    override fun onError(error: Image?) {
+        if (error != null) {
+            view.setImageDrawable(error.asDrawable(view.context.resources))
+        }
     }
 }
 
